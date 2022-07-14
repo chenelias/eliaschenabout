ac1a5e91c4a52c05961b33867459f8b02dea6221
sunlight-git <76611085+chenelias@users.noreply.github.com>
2022-07-14 20:48:52 +0800

    main

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,8 +6,8 @@ export default function Card() {
     <div className="card--main">
         <img className="card--img" src={require("../img/catimg.jpg")} />
         <h1 className="card--title">EliasChen</h1>
-        <p className="card--content">Hello there I'm EliasChen , I love learning English Chinese Math Information-Security programming-language and every thing happens around me and my best friend Ruby(cat). 
-        <br/>Coding is fun and It also gives me a lots of achievement then you well have more power to keep learning it. Hope this happens on yourself!</p>
+        <p className="card--content">Hello there I'm EliasChen , I love learning English Information-Security programming-language and every thing happens around me and my best friend Ruby(cat). 
+        <br/>I start self-study when I was grade five , coding is fun and it also gives me a lots of achievement then i will have more power to keep learning it. Hope this happens on yourself!</p>
         <div className="card--link">
         <a className="card--herf-fb" target="_blank" href="https://www.facebook.com/profile.php?id=100074943037854">Facebook Profile</a>
         <a className="card--herf-ig" target="_blank" href="https://www.instagram.com/codechen9810/">Instagram Profile</a>
